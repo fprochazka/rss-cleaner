4e5106c6ca5b7f232b67342daca77a7baf9f0884
Filip Procházka <filip@prochazka.su>
2016-03-03 22:28:41 +0100

    add two-guys-and-a-guy

---
+++ app/model/rules/.twogag.com.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+	'grabber' => [
+		'%.*%' => [
+			'test_url' => 'http://www.twogag.com/archives/3641',
+			'body' => [
+				'//div[@id="comic"]',
+			],
+			'strip' => [
+			],
+		],
+	],
+];
